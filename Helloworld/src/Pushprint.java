12db9c8b28893a7623f940d5f931a1393b554bed
parkdahyun <qkrekgus4567@naver.com>
2016-10-13 15:53:14 +0900

    rr

--- Helloworld/src/Pushprint.java
+++ Helloworld/src/Pushprint.java
@@ -4,12 +4,10 @@ public class Pushprint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add java class file push");
-<<<<<<< HEAD
 		System.out.println("world with wiku");
-=======
 		System.out.println("World with 42");
->>>>>>> refs/remotes/origin/master
 
+		
 	}
 
 }
