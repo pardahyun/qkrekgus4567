4c99ffffd7dd004a611176ff367354e8023874b9
parkdahyun <qkrekgus4567@naver.com>
2016-10-13 15:37:37 +0900

    ff

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -4,11 +4,9 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("world's end dancehall");
-<<<<<<< HEAD
+
 		System.out.println("earth's end dancehall");
-=======
 		System.out.println("Hello dance world");
->>>>>>> refs/heads/hotfix
 		// TODO Auto-generated method stub
 
 	}
