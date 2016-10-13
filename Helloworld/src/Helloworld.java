81a6b11b9b565f735b425d9a62d59d5937fc2555
parkdahyun <qkrekgus4567@naver.com>
2016-10-13 15:30:52 +0900

    add print line

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -2,6 +2,8 @@
 public class Helloworld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("world's end dancehall");
 		// TODO Auto-generated method stub
 
 	}
